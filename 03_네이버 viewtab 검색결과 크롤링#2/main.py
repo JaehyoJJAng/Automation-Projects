304927f2e480965f291ac04c0e03b031e2cc4d8c
JaehyoJJang <yshrim12@naver.com>
2023-02-09 17:44:48 +0900

    Scroll Down 기능 구현

--- 03_네이버 viewtab 검색결과 크롤링#2/main.py
+++ 03_네이버 viewtab 검색결과 크롤링#2/main.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup as bs
 import requests as rq
 import os
 import urllib.parse as rep
+import time
 
 class ChromeDriver:
     @staticmethod
@@ -34,47 +35,80 @@ class ViewTab:
     def __init__(self) -> None:
         self.keyword  : str = self.input_keyword()
         self.base_url : str = f'https://search.naver.com/search.naver?where=view&sm=tab_jum&query={rep.quote_plus(self.keyword)}'
+        self.browser  : webdriver.Chrome = ChromeDriver().return_driver()
     
     @staticmethod
-    def get_soup_obj(response:rq.Response)-> bs:
-        return bs(response.text,'html.parser')
+    def get_soup_obj(page_source:str)-> bs:
+        return bs(page_source,'html.parser')
     
     def run(self)-> List[Dict[str,str]]:
         # View List
         view_data_list : List[Dict[str,str]] = list()
         
-        # Open Session
-        with rq.Session() as session:
-            with session.get(url=self.base_url,headers={'User-Agent':'Mozilla/5.0'}) as response:
-                soup : bs = self.get_soup_obj(response=response)
+        # 브라우저 이동
+        self.browser.get(url=self.base_url)
+        self.browser.implicitly_wait(time_to_wait=15)
+            
+        # Scroll Down
+        self.scroll_down(scroll_count=3)
                 
-                # Content Length
-                content_length : int = len(soup.select('div.timeline_cont._svp_item'))
-                
-                for idx in range(content_length):
-                    # View Dict
-                    view_data_dict : Dict[str,str] = dict()
-                    
-                    # Get Contents
-                    contents : list = soup.select('div.timeline_cont._svp_item')
-                    
-                    # Get Title & Get Link
-                    title : str = contents[idx].select_one('a.api_txt_lines.total_tit')
-                    if title == None or title.text == '':
-                        title = '-'
-                        link = '-'
-                    else:
-                        link : str = title.attrs['href']
-                        title = title.text.strip()
-                    
-                    # Save Data
-                    view_data_dict['title'] = title
-                    view_data_dict['link']  = link
-                    view_data_list.append(view_data_dict)
+        soup : bs = self.get_soup_obj(page_source=self.browser.page_source)
+        
+        # Content Length
+        content_length : int = len(soup.select('div.timeline_cont._svp_item'))
+        
+        for idx in range(content_length):
+            # View Dict
+            view_data_dict : Dict[str,str] = dict()
+            
+            # Get Contents
+            contents : list = soup.select('div.timeline_cont._svp_item')
+            
+            # Get Title & Get Link
+            title : str = contents[idx].select_one('a.api_txt_lines.total_tit')
+            if title == None or title.text == '':
+                title = '-'
+                link = '-'
+            else:
+                link : str = title.attrs['href']
+                title = title.text.strip()
+            
+            # Save Data
+            view_data_dict['title'] = title
+            view_data_dict['link']  = link
+            view_data_list.append(view_data_dict)
+            
+            # Print Data
+            print(view_data_dict,'\n')
         # Return Data
         return view_data_list
+
+    def scroll_down(self,scroll_count:int=3)-> None:
+        # Clear Console
+        os.system('clear')
+        
+        """ Browser Scroll Down """
+        prev_height = self.browser.execute_script("return document.documentElement.scrollHeight")
+
+        while scroll_count > 0:
+            # 스크롤 내리기
+            self.browser.execute_script("window.scrollTo(0 , document.documentElement.scrollHeight)")
+
+            print(f'scrolling down ({scroll_count})')
+            scroll_count -= 1
+
+            # 대기시간 할당하기
+            time.sleep(2)
+
+            # 새로운 높이 값 받기
+            curr_height = self.browser.execute_script("return document.documentElement.scrollHeight")
+
+            # 이전 높이가 현재 높이와 같은경우 더보기 버튼 클릭
+            if curr_height == prev_height:
+                break
+
+            prev_height = curr_height
     
-    def 
     def input_keyword(self)-> str:
         os.system('clear')
         while True:
