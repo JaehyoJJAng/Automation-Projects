c741184b6554f6a3138a354768a31fb82e23e8b2
JaehyoJJang <yshrim12@naver.com>
2023-02-13 13:26:27 +0900

    feat: HTML 템플릿 가져오기 - f-string 활용하여 index.html 커스텀

--- 12_쿠팡 제품 크롤링 정보로 웹사이트 배포하기/html_maker.py
+++ 12_쿠팡 제품 크롤링 정보로 웹사이트 배포하기/html_maker.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 
 def create_dir(file_path:str):
@@ -8,12 +9,84 @@ def html_maker(file_name:str,file_path:str)-> None:
     # Create Directory
     create_dir(file_path=file_path)
     
-    # HTML Text
-    html_text : str = '<h1>HTML 생성하기</h1>'
+    # Title
+    html_title : str = f'오늘의 {"자전거"}'
+    
+    # Product Name
+    html_prod_name : str = f'오늘의 {"자전거"}'
+    
+    # Date
+    html_today : str = datetime.now().strftime('%Y%m%d')
+    
+    # Sub Text
+    html_sub_text : str  = f'{html_today} 오늘의 {"자전거"} 인기상품 Top10 입니다.'
+    
+    # Main Text
+    html_main_text : str = f"<p>{'test' * 30}</p>"
+    
+    # HTML
+    html : str = f"""
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>{html_title}</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<a href="index.html" class="logo">Massively</a>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Post -->
+							<section class="post">
+								<header class="major">
+									<span class="date">{html_today}</span>
+									<h1>{html_prod_name}</h1>
+									<p>{html_sub_text}</p>
+								</header>
+								<div class="image main"><img src="images/pic01.jpg" alt="" /></div>
+								{html_main_text}
+							</section>
+					</div>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>"""
     
     # Text Write To File
     with open(os.path.join(file_path,file_name)) as fp:
-        fp.write(html_text)
+        fp.write(html)
 
 def main():
     file_name : str = 'index.html'
