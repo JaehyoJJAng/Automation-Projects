35ac3ea747b9e185787481add2df810f4eaff7ad
JaehyoJJang <yshrim12@naver.com>
2023-02-13 13:37:31 +0900

    feat: Coupang 크롤러 구현

---
+++ 12_쿠팡 제품 크롤링 정보로 웹사이트 배포하기/src/config/config.py
@@ -0,0 +1,11 @@
+from typing import Dict
+import os
+import json
+
+def get_headers():
+    with open('headers/headers.json','r') as fp:
+        headers : Dict[str,str] = json.loads(fp.read())    
+    try:
+        return headers
+    except: 
+        raise EnvironmentError()
