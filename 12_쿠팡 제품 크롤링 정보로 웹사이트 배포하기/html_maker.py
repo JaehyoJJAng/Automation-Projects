77f29f009cbb10186712f7ed446e598a869326e0
JaehyoJJang <yshrim12@naver.com>
2023-02-13 12:59:56 +0900

    upd: 디렉토리 생성 함수 분리 , file_path 수정

--- 12_쿠팡 제품 크롤링 정보로 웹사이트 배포하기/html_maker.py
+++ 12_쿠팡 제품 크롤링 정보로 웹사이트 배포하기/html_maker.py
@@ -1,16 +1,23 @@
 import os
 
-def html_maker(file_name:str,file_path:str)-> None:
+def create_dir(file_path:str):
     if not os.path.exists(file_path):
-        os.mkdir(file_path)
+        os.mkdir(file_path)    
 
+def html_maker(file_name:str,file_path:str)-> None:
+    # Create Directory
+    create_dir(file_path=file_path)
+    
+    # HTML Text
     html_text : str = '<h1>HTML 생성하기</h1>'
+    
+    # Text Write To File
     with open(os.path.join(file_path,file_name)) as fp:
         fp.write(html_text)
 
 def main():
     file_name : str = 'index.html'
-    file_path : str = 'html/'
+    file_path : str = 'html5up-massively'
     
     html_maker(file_name=file_name,file_path=file_path)
 
